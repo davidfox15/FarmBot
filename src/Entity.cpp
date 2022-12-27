87199e30c67fd06ee7b8969217af6405d69a7feb
Давид Лисицын <David>
2022-12-27 20:38:05 +0300

    create Entiity class methods

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -2,4 +2,38 @@
 
 Entity::Entity(HANDLE handle, uintptr_t address_entity)
 {
+	this->max_hp = Param(handle, address_entity, PLAYER_MAX_HP_OFFSETS, false);
+	this->hp = Param(handle, address_entity, PLAYER_HP_OFFSETS, false);
+	this->x = Param(handle, address_entity, PLAYER_X_OFFSETS, true);
+	this->y = Param(handle, address_entity, PLAYER_Y_OFFSETS, true);
+}
+
+void Entity::Set(Param x, Param y, Param max_hp, Param hp) {
+	this->x = x;
+	this->y = y;
+	this->max_hp = max_hp;
+	this->hp = hp;
+}
+
+int Entity::GetX() {
+	return x.GetValue();
+}
+
+int Entity::GetY() {
+	return y.GetValue();
+}
+
+int Entity::GetMaxHP() {
+	return max_hp.GetValue();
+}
+
+int Entity::GetHP() {
+	return hp.GetValue();
+}
+
+void Entity::Print() {
+	printf("PlayerMaxHP [%i]\n", GetMaxHP());
+	printf("PlayerHP [%i]\n", GetHP());
+	printf("PlayerX [%i]\n", GetX());
+	printf("PlayerY [%i]\n", GetY());
 }
--- src/Entity.h
+++ src/Entity.h
@@ -12,5 +12,13 @@ class Entity
 	Param y;
 	Param max_hp;
 	Param hp;
+public:
+	Entity(HANDLE handle, uintptr_t address_entity);
+	void Set(Param x, Param y, Param max_hp, Param hp);
+	int GetX();
+	int GetY();
+	int GetMaxHP();
+	int GetHP();
+	void Print();
 };
 
